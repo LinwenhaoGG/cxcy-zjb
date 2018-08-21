02eba6fcad5672b93f7ad393b4753e057ae4d142
DESKTOP-I6S2354\LINWENHAO <617803234@qq.com>
2018-08-21 08:37:22 +0800

    修改一个不能跳转页面跟强制要求前端页面格式的小bug

--- src/main/java/com/cxcy/zjb/springboot/controller/MatchController.java
+++ src/main/java/com/cxcy/zjb/springboot/controller/MatchController.java
@@ -1,11 +1,29 @@
 package com.cxcy.zjb.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 比赛控制层
  * Created by LINWENHAO on 2018/8/20.
  */
 @Controller
+@RequestMapping("/match")
 public class MatchController {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "/match/index";
+    }
+
+
+    @RequestMapping("/a")
+    public ModelAndView listUsers() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/match/wysiwyg");
+        return mav;
+    }
 }
