3bbf53d2168d6086a6f792f1ffa9265ed9e666c0
kolo <807417141@qq.com>
2018-08-29 23:00:23 +0800

    登录注册、聊天模块页面

---
+++ src/main/resources/static/js/friendList.js
@@ -0,0 +1,36 @@
+/*页面加载完成*/
+$(function () {
+    var userId;
+
+    $.ajax({
+        type:'post',
+        url:'/user/person',
+        dataType:'json',
+        async: false,
+        data:{},
+        success:function (data) {
+            userId = data.id;
+        }
+    })
+
+    $.ajax({
+        type:'post',
+        url:'/user/message',
+        dataType:'json',
+        async: false,
+        data:{"userId":userId},
+        success:function (data) {
+            var totalRedDot = 0 ;
+            var arr = data.object;
+            $(arr).each(function () {
+                var per = $(this)[0];
+                var img = "/img/"+per.userImage;
+                $("#showmessage").append("<span class=\"innerspan\"><a href=\"chat.html?userId="+per.userId+"\"><img src=\""+img+"\" class=\"imgClass\"><span>"+per.userName+"</span>    未读消息:<span  style=\"color: red;\">"+per.messageNum+"</span> </a></span>");
+                totalRedDot = totalRedDot+per.messageNum;
+            });
+            $("#totalRedDot").html(totalRedDot);
+
+        }
+    })
+
+})
